c9b09f9ec809122d0bf900f20c4a54d3f0160d87
Nic Newdigate <nicnewdigate@gmail.com>
2020-03-11 20:03:36 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -v
 mkdir output
-python -m kicad-automation.eeschema.schematic export /$(echo "$1")/$(echo "$2") output --file_format svg --all-pages True
-python -m kicad-automation.eeschema.schematic export /$(echo "$1")/$(echo "$2") output --file_format pdf --all-pages True
+python -m kicad-automation.eeschema.schematic export /$(echo "$1")/$(echo "$2") output 
 pwd
 ls -lrt
 echo "Hello $1 $2"
