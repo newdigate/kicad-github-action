d90d6ede08c7fddb1732fece8cd41dc7b20e178b
Nic Newdigate <nicnewdigate@gmail.com>
2020-03-11 23:40:33 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -v
 mkdir output
 #cd $(echo "$1")
+ls /usr/lib/python2.7/dist-packages/kicad-automation/
 python2 -m kicad-automation.eeschema.schematic export /github/workspace/$(echo "$1")/$(echo "$2") output 
 ls -lrt
 echo "Hello $1 $2"
