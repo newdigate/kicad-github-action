6fe5a9fd386e75993d71c5b5d7a0cc51aaaef8d4
nic newdigate <nicnewdigate@gmail.com>
2020-03-11 18:45:58 +0000

    minor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
-python -m kicad-automation.eeschema.schematic export $1/$2.sch . svg all-pages True
-python -m kicad-automation.eeschema.schematic export $1/$2.sch . pdf all-pages True
+python -m kicad-automation.eeschema.schematic export $1/$2 ./ svg all-pages True
+python -m kicad-automation.eeschema.schematic export $1/$2 ./ pdf all-pages True
 pwd
 ls -lrt
 echo "Hello $1 $2"
