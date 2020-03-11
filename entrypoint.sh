94d0be567b82c8784731b714820b3a7ebf9266fc
Nic Newdigate <nicnewdigate@gmail.com>
2020-03-11 19:04:05 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/sh -l
-python -m kicad-automation.eeschema.schematic export "$1/$2" output svg all-pages True
-python -m kicad-automation.eeschema.schematic export "$1/$2" output pdf all-pages True
+#!/bin/sh
+echo on
+python -m kicad-automation.eeschema.schematic export "/$1/$2" output svg all-pages True
+python -m kicad-automation.eeschema.schematic export "/$1/$2" output pdf all-pages True
 pwd
 ls -lrt
 echo "Hello $1 $2"
