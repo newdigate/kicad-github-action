cc8ad896565f3cd0bdc58200281adb2a79e75ddf
Nic Newdigate <nicnewdigate@gmail.com>
2020-03-11 20:27:31 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ cd boards
 cd mainboard
 ls -lrt
 pwd
-cd ../../..
 mkdir output
 cd $(echo "$1")
 python -m kicad-automation.eeschema.schematic export $(echo "$2") ../../output 
+cd ../../..
 pwd
 ls -lrt
 echo "Hello $1 $2"
