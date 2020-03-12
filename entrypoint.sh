7bb9dd189eb574d83e09ce9edca2cb3c2c1a5592
Nic Newdigate <nicnewdigate@gmail.com>
2020-03-12 10:32:00 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -v
 pwd
-ls -lrt
-#ls /usr/lib/python2.7/dist-packages/kicad-automation/
-#echo PYTHONPATH=$PYTHONPATH
-#export PYTHONPATH=/usr/lib/python2.7/dist-packages/:$PYTHONPATH
+tree
 python2 -m kicad-automation.eeschema.schematic export /github/workspace/$1/$2 output
-cp -r output /github/workspace/output
-ls -lrt
-ls -lrt output/
+#cp -r output /github/workspace/output
+#ls -lrt
+#ls -lrt output/
 #echo "Hello $1 $2"
 time=$(date)
 echo ::set-output name=time::$time
