a9a323d7032fba33333723e13341164d17681a94
Nic Newdigate <nicnewdigate@gmail.com>
2020-03-12 10:00:48 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -v
 mkdir output
-cd $(echo "$1")
-ls /usr/lib/python2.7/dist-packages/kicad-automation/
-echo PYTHONPATH=$PYTHONPATH
-export PYTHONPATH=/usr/lib/python2.7/dist-packages/:$PYTHONPATH
+cd /home/runner/work/teensy-eurorack/teensy-eurorack/$(echo "$1")
+#ls /usr/lib/python2.7/dist-packages/kicad-automation/
+#echo PYTHONPATH=$PYTHONPATH
+#export PYTHONPATH=/usr/lib/python2.7/dist-packages/:$PYTHONPATH
 python2 -m kicad-automation.eeschema.schematic export $(echo "$2") output
-cp -r output ../../../output
-cd /github/workspace/
+cp -r output /home/runner/work/teensy-eurorack/teensy-eurorack/output
+cd /home/runner/work/teensy-eurorack/teensy-eurorack/
 ls -lrt
-echo "Hello $1 $2"
+ls -lrt output/
+#echo "Hello $1 $2"
 time=$(date)
 echo ::set-output name=time::$time
