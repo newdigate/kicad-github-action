bc8c56eff8b75310dceb79a4b745b4add35b033c
Nic Newdigate <nicnewdigate@gmail.com>
2020-03-13 10:58:52 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -v
-python2 -m kicad-automation.eeschema.schematic export /github/workspace/$1/$2 output
-python2 -m kicad-automation.eeschema.schematic export -f pdf /github/workspace/$1/$2 output
-python2 -m kicad-automation.eeschema.schematic run_erc /github/workspace/$1/$2 output
+python2 -m kicad-automation.eeschema.schematic export /github/workspace/$1/$2 output/schematic/svg
+python2 -m kicad-automation.eeschema.schematic export -f pdf /github/workspace/$1/$2 output/schematic
+python2 -m kicad-automation.eeschema.schematic run_erc /github/workspace/$1/$2 output/schematic
 
-python2 -m kicad-automation.pcbnew_automation.run_drc /github/workspace/$1/$3 output
-python2 -m kicad-automation.pcbnew_automation.plot /github/workspace/$1/$3 output/gerbers
-python2 -m kicad-automation.pcbnew_automation.plot -f pdf /github/workspace/$1/$3 output/gerbers
+python2 -m kicad-automation.pcbnew_automation.run_drc /github/workspace/$1/$3 output/pcb --record
+python2 -m kicad-automation.pcbnew_automation.plot /github/workspace/$1/$3 output/pcb/gerbers
+python2 -m kicad-automation.pcbnew_automation.plot -f pdf /github/workspace/$1/$3 output/pcb/gerbers
 
 tree
 echo "Hello $1 $2 $3"
