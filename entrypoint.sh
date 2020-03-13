8980fdac09f6e86988669f8d3d9c9042419393fc
Nic Newdigate <nicnewdigate@gmail.com>
2020-03-13 09:20:29 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -v
 python2 -m kicad-automation.eeschema.schematic export /github/workspace/$1/$2 output
+python2 -m kicad-automation.eeschema.schematic export -f pdf /github/workspace/$1/$2 output
+python2 -m kicad-automation.eeschema.schematic run_erc /github/workspace/$1/$2 output
+
+python2 -m kicad-automation.pcbnew_automation.run_drc /github/workspace/$1/$3 output
+python2 -m kicad-automation.pcbnew_automation.plot /github/workspace/$1/$3 output/gerbers
+python2 -m kicad-automation.pcbnew_automation.plot -f pdf /github/workspace/$1/$3 output/gerbers
+
 tree
 echo "Hello $1 $2 $3"
 time=$(date)
