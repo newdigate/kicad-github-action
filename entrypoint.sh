d32443b71464cd01d8699f4c57464ca1ebbe8536
Nic Newdigate <nicnewdigate@gmail.com>
2020-07-22 21:33:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 python2 -m kicad-automation.eeschema.schematic export /github/workspace/$1/$2 output/schematic/svg
 python2 -m kicad-automation.eeschema.schematic export -f pdf /github/workspace/$1/$2 output/schematic
 python2 -m kicad-automation.eeschema.schematic run_erc /github/workspace/$1/$2 output/schematic
-
-python2 -m kicad-automation.pcbnew_automation.run_drc /github/workspace/$1/$3 output/pcb --record
+python2 -m kicad-automation.pcbnew_automation.run_drc /github/workspace/$1/$3 output/pcb
+#python2 -m kicad-automation.pcbnew_automation.run_drc /github/workspace/$1/$3 output/pcb --record
 python2 -m kicad-automation.pcbnew_automation.plot /github/workspace/$1/$3 output/pcb/gerbers
 python2 -m kicad-automation.pcbnew_automation.plot -f pdf /github/workspace/$1/$3 output/pcb/gerbers
 
-tree
+#tree
 echo "Hello $1 $2 $3"
 time=$(date)
 echo ::set-output name=time::$time
