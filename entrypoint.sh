8e0a71010388472df973f67ee41251875501dc89
Nic Newdigate <nicnewdigate@gmail.com>
2020-07-23 17:20:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ if [ $retval -ne 0 ]; then
     echo ::set-output name=success::0
     echo ::set-output name=message::"ERC failed"
     exit 0;
+else
+    rm output/schematic/run_erc_schematic_screencast.ogv
 fi
 
 python2 -m kicad-automation.pcbnew_automation.run_drc /github/workspace/$1/$3 output/pcb --record
@@ -15,10 +17,15 @@ if [ $retval -ne 0 ]; then
     echo ::set-output name=success::0
     echo ::set-output name=message::"DRC failed"
     exit 0;
+else
+    rm output/pcb/run_drc_screencast.ogv
 fi
 
 python2 -m kicad-automation.eeschema.schematic export /github/workspace/$1/$2 output/schematic/svg
 python2 -m kicad-automation.eeschema.schematic export -f pdf /github/workspace/$1/$2 output/schematic
+rm output/schematic/svg/export_schematic_screencast.ogv
+rm output/schematic/export_schematic_screencast.ogv
+
 python3 -m kicad-automation.pcbnew_automation.plot /github/workspace/$1/$3 output/pcb/gerbers
 python3 -m kicad-automation.pcbnew_automation.plot -f pdf /github/workspace/$1/$3 output/pcb/gerbers
 
